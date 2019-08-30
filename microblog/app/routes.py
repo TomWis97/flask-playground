e3f1b7a66e01bcb392c1b79124b4c6c7c0f026ba
TomWis97 <github@tomwishaupt.nl>
2019-08-30 15:31:29 +0200

    2: Templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Tom'}
+    posts  = [
+        {
+            'author': {'username': 'John'},
+            'body': "Het is hier echt fantastisch enzo."
+        },
+        {
+            'author': {'username': 'Frank'},
+            'body': "Alles is kut."
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
